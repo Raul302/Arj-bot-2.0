d1f70b3d9e37d810af3ded454d9bb6a195adfedf
Raul302 <Luis_1547@hotmail.com>
2020-04-21 21:11:44 -0500

    AWS ready

--- app/Controllers/Ws/ArjRobotController.js
+++ app/Controllers/Ws/ArjRobotController.js
@@ -1,4 +1,4 @@
-'use strict'
+  'use strict'
 
 class ArjRobotController {
   constructor ({ socket, request }) {
@@ -10,33 +10,31 @@ class ArjRobotController {
  onTest(data)
  {
    console.log('Subscripcion ' + data);
-  this.socket.emit('subscripcion','Hola desde el server');
+   this.socket.broadcastToAll('subscripcion', 'Hola desde el server');
 }
   // Carro
   onAtras(data)
   {
     console.log('Atras ' + data);
-    this.socket.emit('changeAtras','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeAtras',data);
 
   }
   onAdelante(data)
   {
     console.log('Adelante ' + data);
-    this.socket.emit('changeAdelante','Tu data desde el server: ' + data)
-
+    this.socket.broadcastToAll('changeAdelante',data);
   }
   onIzquierda(data)
   {
     console.log('Izquierda ' + data);
-    this.socket.emit('changeIzquierda','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeIzquierda',data);
 
 
   }
   onDerecha(data)
   {
     console.log('Derecha ' + data);
-    this.socket.emit('changeDerecha','Tu data desde el server: ' + data)
-
+    this.socket.broadcastToAll('changeDerecha',data);
 
   }
 
@@ -44,13 +42,13 @@ class ArjRobotController {
   onAgarrar(data)
   {
     console.log('Agarre ' + data);
-    this.socket.emit('changeAgarre','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeAgarre',data);
 
   }
   onSoltar(data)
   {
     console.log('Soltar ' + data);
-    this.socket.emit('changeSoltar','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeSoltar',data);
 
   }
 
@@ -58,26 +56,26 @@ class ArjRobotController {
   onBAbajo(data)
   {
     console.log('Abajo Brazo ' + data);
-    this.socket.emit('changeAbajo','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeBAbajo',data);
 
   }
   onBArriba(data)
   {
     console.log('Arriba Brazo ' + data);
-    this.socket.emit('changeArriba','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeBArriba',data);
 
 
   }
   onBIzquierda(data)
   {
     console.log('Izquierda Brazo ' + data);
-    this.socket.emit('changeIzquierda','Tu data desde el server: ' + data)
+    this.socket.broadcastToAll('changeBIzquierda',data);
 
   }
   onBDerecha(data)
   {
     console.log('Derecha Brazo ' + data);
-    this.socket.emit('changeDerecha','Tu data desde el server ' + data)
+    this.socket.broadcastToAll('changeBDerecha',data);
 
   }
 }
